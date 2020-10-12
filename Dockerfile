f0b17f5b85f0e0f003ce0424b74defc3a5711cdd
Seth Morton <sethmorton@Seths-MacBook-Air.local>
2020-10-12 15:55:45 -0700

    --- Auto-Git Commit ---

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 
 FROM node:14
+COPY . .
+ENTRYPOINT "run.sh && run.sh run"
 
-RUN apt-get update && apt-get -y install cron
-# WORKDIR /app
-# COPY package*.json ./app
-COPY app.js /bin/app.js
-COPY root /var/at/tabs
-RUN chmod +x /bin/app.js
-CMD crontab -e 2 -f
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let shell = require("shelljs");
 const cron = require("node-cron");
-console.log("hello");
-cron.schedule("10 * * * * *", function () {
-  if (shell.exec("node app.js").code !== 0) {
+let shell = require("shelljs");
+cron.schedule("0 * * * * *", function () {
+  if (shell.exec("node backend/app.js").code !== 0) {
     console.log("something went wrong");
   }
 });
